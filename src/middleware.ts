a0a58c443ed64dde79046afa3912771de1bc402d
Abid Al Wassie <abidalwassie@outlook.com>
2024-09-20 12:42:24 +0600

    feat: added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        // check if the user is authorized
+        if (token) return true;
+
+        // If there's no token then redirect to signin page
+        if (req.nextUrl.pathname !== "/api/auth/signin") {
+          return false;
+        }
+
+        return true;
+      },
+    },
+  },
+);
+
+export const config = {
+  matcher: ["/dashboard", "/profile", "/settings", "/contribute"],
+};
